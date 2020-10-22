41270ad9bb3ad199e9b0c700f3f4e475968fa678
mikeSwift <mike2345swift>
2020-10-22 20:56:36 +0800

    init a project

---
+++ src/main/java/zhc/java/Entry.java
@@ -0,0 +1,13 @@
+package zhc.java;
+/*
+* @Author: Marte
+* @Date:   2020-10-22 14:56:52
+* @Last Modified by:   Marte
+* @Last Modified time: 2020-10-22 20:22:31
+*/
+
+public class Entry {
+    public static void main(String[] args) {
+        System.out.println("hello中文 world!");
+    }
+}
